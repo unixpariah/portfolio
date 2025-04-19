41ea39e1d4fbacc293f34838d2314cd6df4091b3
unixpariah <oskar.rochowiak@tutanota.com>
2025-04-19 18:18:12 +0200

    frontend layout

---
+++ client/src/components/main-nav.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { ExternalLink } from "lucide-react";
+import { Icons } from "./icons";
+
+export function MainNav() {
+  const pathname = usePathname();
+
+  return (
+    <div className="hidden mr-4 md:flex">
+      <nav className="flex items-center gap-4 text-sm xl:gap-6">
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/docs/installation"
+              ? "text-foreground"
+              : "text-foreground/80",
+          )}
+        >
+          Home
+        </Link>
+
+        <Link
+          href="/blog"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/blog")
+              ? "text-foreground"
+              : "text-foreground/80",
+          )}
+        >
+          Blog
+        </Link>
+
+        <Link
+          href={siteConfig.links.resume}
+          target="_blank"
+          className="flex gap-1"
+        >
+          Resume <ExternalLink className="size-3" />
+        </Link>
+      </nav>
+    </div>
+  );
+}
---
+++ client/src/config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: "TODO",
+  url: "https://TODO.com",
+  ogImage: "TODO",
+  description: "Portfolio of Oskar Rochowiak",
+  links: {
+    shadcn: "https://ui.shadcn.com/",
+    github: "https://github.com/unixpariah/portfolio",
+    githubProfile: "https://github.com/unixpariah",
+    resume: "https://www.overleaf.com/read/pwbbqtpthxxr#946f81",
+    email: "mailto:oskarrochowiak@gmail.com",
+    githubUsername: "unixpariah",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
